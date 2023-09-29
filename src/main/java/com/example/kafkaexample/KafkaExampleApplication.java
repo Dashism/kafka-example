f78600ff38ca98c8106574bfb1f51a60319a056d
dashism <benoitdarenne@hotmail.fr>
2023-09-29 14:16:29 +0200

    init commit

---
+++ src/main/java/com/example/kafkaexample/KafkaExampleApplication.java
@@ -0,0 +1,26 @@
+package com.example.kafkaexample;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@SpringBootApplication
+public class KafkaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaExampleApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate) {
+		System.out.println("commandLineRunner is here");
+		return args -> {
+			for (int i = 0; i < 10; i++) {
+				//Use this if you don't want to use API call
+				//kafkaTemplate.send("first", "message number " + i + " send to first topic !");
+			}
+		};
+	}
+}
